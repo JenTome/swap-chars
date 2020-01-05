87e5331e60e4efe4a920ee9b588ce94aa6e22fbe
JenTome <tome.jen@gmail.com>
2020-01-05 14:06:02 -0500

    swap 2 chars and then some

---
+++ src/SwapCharsTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapCharsTest {
+
+	@Test
+	void swapStringWith2Chars() {
+		assertEquals("AB",swapchars.swap2("BA"));
+	}
+	@Test
+	void swapStringWith6Chars() {
+		assertEquals("AB",swapchars.swap2("BA"));
+	}
+}
